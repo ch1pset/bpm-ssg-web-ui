e3085b3f5b53373d598602d552167ba6acd85ac6
Josh <joshneilgonzales@gmail.com>
2021-09-13 02:39:49 -0500

    improved save file download

--- src/components/generate/generate.js
+++ src/components/generate/generate.js
@@ -7,17 +7,26 @@ class GenerateButton extends React.Component {
   static contextType = StateContext;
   constructor(props) {
     super(props);
-    this.state = {url:""}
+    this.state = {
+      url:null,
+      filename:""
+    }
     this.handleClick = this.handleClick.bind(this);
   }
   handleClick(e) {
       if(this.context.seed !== "" && this.context.char !== "" && this.context.diff !== "") {
-        const blob = new Blob([bpmssg(this.context)]);
-        const url = URL.createObjectURL(blob);
-        this.setState({url}, () => {
+        console.log(`Generating save for ${JSON.stringify(this.context)}`)
+        let savedata = [bpmssg(this.context).serialize()];
+        let filename = `ContinueStateV2.sav`;
+        let file = new File(savedata, filename);
+        let url = URL.createObjectURL(file);
+        this.setState({url,filename}, () => {
           this.dofileDownload.click();
           URL.revokeObjectURL(url);
-          this.setState({url:""});
+          url = null;
+          file = null;
+          filename = "";
+          this.setState({url,filename}, () => window.location.reload(false))
         })
       } else {
         window.alert(`All fields must be chosen/filled`);
@@ -29,7 +38,7 @@ class GenerateButton extends React.Component {
       <div className="Button" style={styles}>
           <button className="gen" onClick={this.handleClick}>Generate</button>
           <a style={{display: "none"}}
-              download="ContinueStateV2.sav"
+              download={this.state.filename}
               href={this.state.url}
               ref={e=>this.dofileDownload = e}
           >download</a>
