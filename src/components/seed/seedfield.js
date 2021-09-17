758170b36d725596d94f50e68a598db9da980552
Josh <joshneilgonzales@gmail.com>
2021-09-17 00:17:07 -0500

    Update `SeedField` for no context

--- src/components/seed/seedfield.js
+++ src/components/seed/seedfield.js
@@ -1,64 +1,44 @@
 import React from 'react';
-import StateContext from '../context.js';
 import styles from './seedfield.css';
+import xorshift from '../../utils/xorshift';
 
 class SeedField extends React.Component {
-  static contextType = StateContext;
   constructor(props) {
     super(props);
-    this.state = {seed:"enter a seed here..."}
+    this.state = {seed:""}
     this.handleChange = this.handleChange.bind(this);
     this.handleClick = this.handleClick.bind(this);
   }
   handleChange(e) {
     this.setState({seed:e.target.value}, () => {
-      if(this.state.seed === "")
-        this.setState({seed:"enter a seed here..."})
+      this.props.onChange(this.state);
     });
-    this.context.seed = e.target.value;
   }
   handleClick(e) {
     this.setState({seed:this.genSeed(Date.now())}, () => {
-      this.context.seed = this.state.seed;
-      this.setState({seed:this.context.seed})
+      this.setState({seed:this.state.seed});
+      this.props.onChange(this);
     })
-  }
-  xorShift(seed)
-  {
-      let x = seed;
-      x ^= x << 13;
-      x ^= x >>> 7; //bitshift right without preserving sign because JS is dumb
-      x ^= x << 17;
-      return x;
   }
   genSeed(seed)
   {
     let output = "";
-    let cseed = this.xorShift(seed);
-    let i = 0;
-    do {
-      let chars = "0123456789abcdefghijklmnopqrstuvwxyz"
-      cseed = this.xorShift(cseed);
-      output += chars.at(this.xorShift(cseed) % 36)
-      i++
-    } while(i < 20)
+    let chars = "0123456789abcdefghijklmnopqrstuvwxyz"
+    for(let i = 0; i < 20; i++)
+      output += chars.at((seed = xorshift(seed)) % 36);
     return output;
   }
   render() {
     return (
-      <div className="SeedField" styles={styles}>
-        <div>
+      <div styles={styles}>
           <input onChange={this.handleChange} 
-                placeholder={this.state.seed} 
-                value={this.context.seed}
+                placeholder="enter a seed here..."
+                value={this.state.seed}
                 className="Field"/>
-          <button className="Field button" 
+          <button className="button" 
               style={styles} 
               onClick={this.handleClick}
               >Random</button>
-        </div>
-        <div>
-        </div>
       </div>
     );
   }
