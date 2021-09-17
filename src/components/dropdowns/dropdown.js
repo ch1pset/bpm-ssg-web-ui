d5a42c21451931733942802f2962d075043ba5c9
Josh <joshneilgonzales@gmail.com>
2021-09-17 00:11:17 -0500

    Added `Dropdown` component

---
+++ src/components/dropdowns/dropdown.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import styles from './dropdowns.css';
+
+class Dropdown extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {selection:""}
+        this.handleChange = this.handleChange.bind(this);
+    }
+    handleChange(e) {
+        console.log(`Selected ${e.target.value}`)
+        this.setState({selection:e.target.value}, () => {
+            this.props.onChange(this);
+        })
+    }
+    createOptions() {
+        return this.props.options.map((opt, i) => {
+            return <option key={i} value={opt}>{opt}</option>
+        });
+    }
+    render() {
+        return (
+            <div style={styles}>
+                <select className="Dropdown" 
+                    onChange={this.handleChange}>
+                    <option value="">--Choose a {this.props.name}--</option>
+                    {this.createOptions()}
+                </select>
+            </div>
+        );
+    }
+}
+
+export default Dropdown;
