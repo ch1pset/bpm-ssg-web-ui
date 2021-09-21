99f30639351eb93f4c6f3d1d2eab0fe3db6fe751
Josh <joshneilgonzales@gmail.com>
2021-09-21 00:39:53 -0500

    `VersionInfo` now opens changelog on click

--- src/components/version/version.js
+++ src/components/version/version.js
@@ -1,16 +1,36 @@
 import React from 'react';
 import styles from './version.css';
-import application from '../../../package.json'
+import ReactMarkdown from 'react-markdown';
+
+const devMode = process.env.NODE_ENV === 'development';
 
 class VersionInfo extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {version:application.version}
+    this.state = {value:null, active:false}
+    this.handleClick = this.handleClick.bind(this);
+  }
+  componentDidMount() {
+    fetch(devMode ? 'raw/4FKhJjW7' : 'https://pastebin.com/raw/4FKhJjW7')
+        .then(r => r.text())
+        .then(text => {
+          this.update(text, false);
+        })
+  }
+  handleClick(e) {
+    this.update(this.state.value, !this.state.active);
+  }
+  update(value, active) {
+    this.setState({value, active});
   }
   render() {
     return (
-      <div className="VersionInfo" styles={styles}>
-        <label id="version">v{this.state.version}</label>
+      <div styles={styles}>
+        <label className="version" 
+          onClick={this.handleClick} >v{this.props.value}</label>
+        <ReactMarkdown 
+            className={`info${this.state.active ? '-active':''}`}
+            children={this.state.value}/>
       </div>
     );
   }
