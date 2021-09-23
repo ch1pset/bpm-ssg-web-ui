558f90c30f5edeb9c0e9a00667239b5de331d05f
Josh <joshneilgonzales@gmail.com>
2021-09-22 21:38:34 -0500

    `Tooltip` should only show if it has one

--- src/components/tooltip/tooltip.js
+++ src/components/tooltip/tooltip.js
@@ -6,11 +6,14 @@ class Tooltip extends React.Component {
     constructor(props) {
         super(props);
     }
+    show() {
+        return this.props.show && this.props.tooltip !== undefined
+    }
     render() {
         return (
             <div style={Styles}>
                 <ReactMarkdown 
-                    className={`tooltip${this.props.show ? '-visible': ''}`} 
+                    className={`tooltip${this.show() ? '-visible': ''}`} 
                     children={this.props.tooltip}/>
             </div>
         )
